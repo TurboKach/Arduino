b7fa9a8506df0b12d4b88566836e77a667344994
TurboKach <incognito@turbokach.me>
2019-04-11 14:30:46 +0300

    Update
    Print to console only when potentiometer value changed

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
-
-int potPin = 0;    // select the input pin for the potentiometer
+int potBrightnessPin = 0;    // select the input pin for the potentiometer
+int potRedPin = 1;    // select the input pin for the potentiometer
+int potGreenPin = 2;    // select the input pin for the potentiometer
+int potBluePin = 3;    // select the input pin for the potentiometer
+int ledPin = 13;   // select the pin for the LED
+unsigned int potBrightnessVal, potBrightnessFirstVal, potRedVal = 0;       // variable to store the value coming from the sensor
 
 void setup() {
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);  // declare the ledPin as an OUTPUT
+  potBrightnessFirstVal = (analogRead(potBrightnessPin) >> 2)*1.13;
+  Serial.println("Setup ");
+  Serial.println(potBrightnessFirstVal);
 }
 
 void loop() {
-  val = analogRead(potPin);    // read the value from the sensor
-  digitalWrite(ledPin, HIGH);  // turn the ledPin on
-  delay(val);                  // stop the program for some time
-  digitalWrite(ledPin, LOW);   // turn the ledPin off
-  delay(val);                  // stop the program for some time
+  potBrightnessVal = (analogRead(potBrightnessPin) >> 2)*1.13; // read the value from the sensor
+  if (potBrightnessVal != potBrightnessFirstVal) {
+    potBrightnessFirstVal = potBrightnessVal;
+    Serial.println(potBrightnessVal);
+  }
 }
