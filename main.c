dc523fb179555723b6a40d86dbd34b8c16c18d1e
TurboKach <incognito@turbokach.me>
2019-04-11 15:37:02 +0300

    Update to v1.0
    Released version 1.0!
    Features:
    -RGBW color change by 4 potentiometers

--- main.c
+++ main.c
@@ -1,65 +1,77 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
 
-/* Analog Read to LED
-   ------------------
+#define LED_PIN     6
 
-   turns on and off a light emitting diode(LED) connected to digital
-   pin 13. The amount of time the LED will be on and off depends on
-   the value obtained by analogRead(). In the easiest case we connect
-   a potentiometer to analog pin 2.
+#define LED_COUNT  144
 
-   Created 1 December 2005
-   copyleft 2005 DojoDave <http://www.0j0.org>
-   http://arduino.berlios.de
+// NeoPixel brightness, 0 (min) to 255 (max)
+#define BRIGHTNESS 50
 
-*/
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
-int potBrightnessPin = 0;    // select the input pin for the potentiometer
+int potWhitePin = 0;    // select the input pin for the potentiometer
 int potRedPin = 1;    // select the input pin for the potentiometer
 int potGreenPin = 2;    // select the input pin for the potentiometer
 int potBluePin = 3;    // select the input pin for the potentiometer
 int ledPin = 13;   // select the pin for the LED
-unsigned int potBrightnessVal, potBrightnessFirstVal; // variable to store the value coming from the sensor
+unsigned int potWhiteVal, potWhiteFirstVal; // variable to store the value coming from the sensor
 unsigned int potRedVal, potRedFirstVal;       // variable to store the value coming from the sensor
 unsigned int potGreenVal, potGreenFirstVal;       // variable to store the value coming from the sensor
 unsigned int potBlueVal, potBlueFirstVal;       // variable to store the value coming from the sensor
 
 void setup() {
-  Serial.begin(9600);
-  potBrightnessFirstVal = (analogRead(potBrightnessPin) >> 2)*1.13;
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(BRIGHTNESS); // Set BRIGHTNESS to about 1/5 (max = 255)
+  
+  potWhiteFirstVal = (analogRead(potWhitePin) >> 2)*1.13;
   potRedFirstVal = (analogRead(potRedPin) >> 4)*4*1.14;
   potGreenFirstVal = (analogRead(potGreenPin) >> 4)*4*1.14;
   potBlueFirstVal = (analogRead(potBluePin) >> 4)*4*1.14;
-  Serial.println("Setup begins: ");
-  Serial.println(potBrightnessFirstVal);
-  Serial.println(potRedFirstVal);
-  Serial.println(potGreenFirstVal);
-  Serial.println(potBlueFirstVal);
-  Serial.println("Setup ends. ");
+  
+  strip.Color(255, 0, 0); // Red
+  strip.show(); 
 }
 
 void loop() {
-  potBrightnessVal = (analogRead(potBrightnessPin) >> 2)*1.13; // read the value from the sensor
-  if (potBrightnessVal != potBrightnessFirstVal) {
-    potBrightnessFirstVal = potBrightnessVal;
-    Serial.println("Brightness: ");
-    Serial.println(potBrightnessVal);
+  // Fill along the length of the strip in various colors...
+  strip.fill(strip.Color(potRedFirstVal, potGreenFirstVal, potBlueFirstVal, potWhiteFirstVal));
+  strip.show();
+  //Potentiometer code BEGINS
+  potWhiteVal = (analogRead(potWhitePin) >> 2)*1.13; // read the value from the sensor
+  if (potWhiteVal != potWhiteFirstVal) {
+    potWhiteFirstVal = potWhiteVal;
   }
   potRedVal = (analogRead(potRedPin) >> 4)*4*1.14; // read the value from the sensor
   if (potRedVal != potRedFirstVal) {
     potRedFirstVal = potRedVal;
-    Serial.println("Red: ");
-    Serial.println(potRedVal);
   }
   potGreenVal = (analogRead(potGreenPin) >> 4)*4*1.14; // read the value from the sensor
   if (potGreenVal != potGreenFirstVal) {
     potGreenFirstVal = potGreenVal;
-    Serial.println("Green: ");
-    Serial.println(potGreenVal);
   }
   potBlueVal = (analogRead(potBluePin) >> 4)*4*1.14; // read the value from the sensor
   if (potBlueVal != potBlueFirstVal) {
     potBlueFirstVal = potBlueVal;
-    Serial.println("Blue: ");
-    Serial.println(potBlueVal);
   }
+  //Potentiometer code ENDS
+
+  // Fill along the length of the strip in various colors...
+  
 }
